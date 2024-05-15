a2640bce6702bb33605b786de5fd5764340c7294
Kirthikadhavamani <kiruthikadhavamani01122001@gmail.com>
2024-05-15 22:07:59 +0530

    okie

---
+++ first.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <math.h>
+void main(){
+    float x;
+    printf("Enter the value of x:");
+    scanf("%f",&x);
+    printf("square root of %6.2f is %6.2f",x,sqrt(x));
+}
