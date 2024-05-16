7443ba8324c3f37bdc85e2141cd3e5263985121c
Kirthikadhavamani <kiruthikadhavamani01122001@gmail.com>
2024-05-16 22:52:33 +0530

    OKIE

---
+++ fs.c
@@ -0,0 +1,44 @@
+// Online C compiler to run C program online
+
+// calculate the value of string
+#include <stdio.h>
+int main(){ 
+    int a=3;
+    if(a%2==0){
+       printf("even");  
+    }
+    else{
+        printf("odd");
+    }
+    
+    return 0;
+    
+}
+
+
+
+// Online C compiler to run C program online
+
+// calculate the value of string
+#include <stdio.h>
+int main(){ 
+  int a,b,c;
+  printf("Enter the three number :");
+  scanf("%d %d %d",&a,&b,&c);
+  if(a>b && a>c){
+      printf("The greatest number is a",a);
+  }
+   else if(b>a && b>c){
+      printf("The greatest number is b",b);
+  }
+    else{
+        printf("The greatest number is c : %d ",c);
+    }
+    
+    return 0;
+    
+}
+
+
+
+
